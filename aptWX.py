8c64fac57e5f8ba8b495ae33282be53723388858
Joseph L <56083316+ianlibasora@users.noreply.github.com>
2020-09-15 16:54:47 +0100

    tracker change + metar (non async)

--- aptWX.py
+++ aptWX.py
@@ -3,12 +3,38 @@
 import requests
 from bs4 import BeautifulSoup
 
+# convert to async for discord pot
 
 
+
+class Weather(object):
+   # uri = f"https://aviationweather.gov/metar/data?ids={APT}"
+
+   @staticmethod
+   def metar(APT="EIDW"):
+      """Default airport set to Dublin (EIDW)"""
+
+      uri = f"https://aviationweather.gov/metar/data?ids={APT}"
+      while True:
+         web = requests.get(uri)
+         if web.ok:
+            soup = BeautifulSoup(web.text, "html.parser")
+            try:
+               return soup.code.text
+            except AttributeError:
+               print("Error occured")
+               continue
+         else:
+            print(f"Web error occured. code {web.status_code}")
+            break
 
 
 def main():
-   pass
+   print(" --- Airport METAR / TAF testing --- ")
+
+   print(Weather.metar("EGLL"))
+
+
 
 
 if __name__ == "__main__":
