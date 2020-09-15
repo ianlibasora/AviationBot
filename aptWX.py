f0dd7f1885e4540ad8fbdcd1095fdefff77e82b1
Joseph L <56083316+ianlibasora@users.noreply.github.com>
2020-09-15 17:02:32 +0100

    metar + taf w/o async

--- aptWX.py
+++ aptWX.py
@@ -8,13 +8,37 @@ from bs4 import BeautifulSoup
 
 
 class Weather(object):
-   # uri = f"https://aviationweather.gov/metar/data?ids={APT}"
+   """Aviation METAR/TAF request lib"""
 
    @staticmethod
    def metar(APT="EIDW"):
-      """Default airport set to Dublin (EIDW)"""
+      """
+      Returns requested airport METAR
+      Default airport set to Dublin (EIDW)
+      """
 
       uri = f"https://aviationweather.gov/metar/data?ids={APT}"
+      while True:
+         web = requests.get(uri)
+         if web.ok:
+            soup = BeautifulSoup(web.text, "html.parser")
+            try:
+               return soup.code.text
+            except AttributeError:
+               print("Error occured")
+               continue
+         else:
+            print(f"Web error occured. code {web.status_code}")
+            break
+
+   @staticmethod
+   def taf(APT="EIDW"):
+      """
+      Returns requested airport TAF
+      Default airport set to Dublin (EIDW)
+      """
+
+      uri = f"https://aviationweather.gov/taf/data?ids={APT}"
       while True:
          web = requests.get(uri)
          if web.ok:
@@ -33,10 +57,9 @@ def main():
    print(" --- Airport METAR / TAF testing --- ")
 
    print(Weather.metar("EGLL"))
-
-
+   print(Weather.metar("KLAX"))
+   print(Weather.metar())
 
 
 if __name__ == "__main__":
    main()
-
