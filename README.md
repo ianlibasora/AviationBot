e41886b1c892eb1616d4810b07ffad4cdbcfe46b
Joseph L <56083316+ianlibasora@users.noreply.github.com>
2020-09-15 16:24:25 +0100

    init all files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Discord Aviation Bot
+
+## WIP
+
+Planned:
+- METAR requests
+- TAF request
+- Flight status requests
---
+++ aptWX.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+
+def main():
+   pass
+
+
+if __name__ == "__main__":
+   main()
+
